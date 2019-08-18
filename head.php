2c45af95ceb03c47655c26bde8a812245de9f39a
Yegerq <54216336+Yegerq@users.noreply.github.com>
2019-08-19 00:55:25 +0300

    Add files via upload

---
+++ head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>!!#!#!ment</title>
+    <link rel="stylesheet" type="text/css" href="/public/css/style.css">
+
+</head>
+
+
+<body>
+      <?php require('nav.php'); ?>
